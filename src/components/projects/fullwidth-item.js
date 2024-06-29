0162e07c13105a72620921e4a8fa8b3c886699dc
rahulb03 <122718029+rahulb03@users.noreply.github.com>
2024-06-29 15:39:47 +0530

    commit

--- src/components/projects/fullwidth-item.js
+++ src/components/projects/fullwidth-item.js
@@ -1,116 +1,152 @@
-//  /* eslint-disable @next/next/no-img-element */
+// // //  /* eslint-disable @next/next/no-img-element */
+// // // import PropTypes from 'prop-types';
+// // // import Link from 'next/link';
+// // // import { Col } from 'react-bootstrap';
+// // // import Image from 'next/image';
+
+// // // function ProjectFullwidthItem({ project }) {
+// // //     const imagePath = `/images/projects/${project?.slug}/${project?.image}`;
+
+// // //     return (
+// // //         <Col lg={{ span: 4 }} md={{ span: 6 }}>
+// // //              <div className="project-item">
+// // //                      <Image src={imagePath} alt={project?.title} 
+// // //                        height={501}
+// // //                        width={366}
+// // //                     />
+// // //              </div>
+// // //          </Col>
+// // //     );
+// // // }
+
+// // // ProjectFullwidthItem.propTypes = {
+// // //     project: PropTypes.instanceOf(Object).isRequired,
+// // // };
+
+// // // export default ProjectFullwidthItem;
+
+
+// // /* eslint-disable @next/next/no-img-element */
+// // // import PropTypes from 'prop-types';
+// // // import Link from 'next/link';
+// // // import { Col } from 'react-bootstrap';
+// // // import Image from 'next/image';
+// // // import { fullwidth } from './fullwidth-itemimage.js';
+// // // function ProjectFullwidthItem({  }) {
+// // //     // const imagePath = `/images/projects/${project?.slug}/${project?.image}`;
+
+// // //     return (
+// // //         <Col lg={{ span: 4 }} md={{ span: 6 }}>
+// // //             <div className="project-item">
+          
+// // //             {fullwidth.map((src, index) => (
+// // //                     <div key={index}>
+// // //           <Image  src={src}    alt={`Image ${index}`}   
+// // //                       height={501}
+// // //                        width={366} />
+// // //             </div>
+// // //       ))}
+// // //             </div>
+// // //         </Col>
+// // //     );
+// // // }
+
+// // // ProjectFullwidthItem.propTypes = {
+// // //     project: PropTypes.instanceOf(Object).isRequired,
+// // // };
+
+// // // export default ProjectFullwidthItem;
+
+
+
+// // import PropTypes from 'prop-types';
+// // import { Col } from 'react-bootstrap';
+// // import Image from 'next/image';
+// // import fullwidth from './fullwidth-itemimage.js';
+
+// // function ProjectFullwidthItem({ project }) {
+// //     return (
+// //         <Col>
+// //             <div className="project-item" style={{marginTop:'20px', marginLeft:'200px', marginRight:'200px', marginBottom:'30px ' ,display:'flex' , justifyContent: 'center ' , alignItems:'center'}}>
+// //                 <div className="grid-container" style={{display:'grid' , gridTemplateColumns: 'repeat(3,1fr)' , gap:'20px'}}>
+// //                     {fullwidth.map((src, index) => (
+// //                         <div key={index} className="grid-item" style={
+// //                             {display:'flex' , justifyContent: 'center ' , alignItems:'center'}
+// //                         }>
+// //                             <Image src={src} alt='roman' height={501} width={366} />
+// //                         </div>
+// //                     ))}
+// //                 </div>
+// //             </div>
+// //         </Col>
+// //     );
+// // }
+
+
+
+// // export default ProjectFullwidthItem;
+
+
+
 // import PropTypes from 'prop-types';
-// import Link from 'next/link';
-// import { Col } from 'react-bootstrap';
+// import { Col, Container, Row } from 'react-bootstrap';
 // import Image from 'next/image';
+// import fullwidth from './fullwidth-itemimage.js';
 
 // function ProjectFullwidthItem({ project }) {
-//     const imagePath = `/images/projects/${project?.slug}/${project?.image}`;
-
 //     return (
-//         <Col lg={{ span: 4 }} md={{ span: 6 }}>
-//              <div className="project-item">
-//                      <Image src={imagePath} alt={project?.title} 
-//                        height={501}
-//                        width={366}
-//                     />
-//              </div>
-//          </Col>
+//         <Container>
+//             <Row>
+//                 <Col className="project-item my-3">
+//                     <div className="grid-container d-flex justify-content-center align-items-center">
+//                         <Row className="w-100">
+//                             {fullwidth.map((src, index) => (
+//                                 <Col key={index} xs={12} sm={6} md={4} className="grid-item mb-3 d-flex justify-content-center align-items-center">
+//                                     <Image src={src} alt='roman' height={501} width={366} className="img-fluid" />
+//                                 </Col>
+//                             ))}
+//                         </Row>
+//                     </div>
+//                 </Col>
+//             </Row>
+//         </Container>
 //     );
 // }
 
 // ProjectFullwidthItem.propTypes = {
-//     project: PropTypes.instanceOf(Object).isRequired,
+//     project: PropTypes.object.isRequired,
 // };
 
 // export default ProjectFullwidthItem;
-
-
-/* eslint-disable @next/next/no-img-element */
-// import PropTypes from 'prop-types';
-// import Link from 'next/link';
-// import { Col } from 'react-bootstrap';
-// import Image from 'next/image';
-// import { fullwidth } from './fullwidth-itemimage.js';
-// function ProjectFullwidthItem({  }) {
-//     // const imagePath = `/images/projects/${project?.slug}/${project?.image}`;
-
-//     return (
-//         <Col lg={{ span: 4 }} md={{ span: 6 }}>
-//             <div className="project-item">
-          
-//             {fullwidth.map((src, index) => (
-//                     <div key={index}>
-//           <Image  src={src}    alt={`Image ${index}`}   
-//                       height={501}
-//                        width={366} />
-//             </div>
-//       ))}
-//             </div>
-//         </Col>
-//     );
-// }
-
-// ProjectFullwidthItem.propTypes = {
-//     project: PropTypes.instanceOf(Object).isRequired,
-// };
-
-// export default ProjectFullwidthItem;
-
 
 
 import PropTypes from 'prop-types';
-import { Col } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Image from 'next/image';
 import fullwidth from './fullwidth-itemimage.js';
 
 function ProjectFullwidthItem({ project }) {
     return (
-        <Col>
-            <div className="project-item" style={{marginTop:'20px', marginLeft:'200px', marginRight:'200px', marginBottom:'30px ' ,display:'flex' , justifyContent: 'center ' , alignItems:'center'}}>
-                <div className="grid-container" style={{display:'grid' , gridTemplateColumns: 'repeat(3,1fr)' , gap:'20px'}}>
-                    {fullwidth.map((src, index) => (
-                        <div key={index} className="grid-item" style={
-                            {display:'flex' , justifyContent: 'center ' , alignItems:'center'}
-                        }>
-                            <Image src={src} alt='roman' height={501} width={366} />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </Col>
+        <Container>
+            <Row>
+                <Col className="project-item my-3">
+                    <div className="grid-container d-flex justify-content-center align-items-center">
+                        <Row className="w-100">
+                            {fullwidth.map((src, index) => (
+                                <Col key={index} xs={6} sm={6} md={4} className="grid-item mb-3 d-flex justify-content-center align-items-center">
+                                    <Image src={src} alt='roman' height={501} width={366} className="img-fluid" priority/>
+                                </Col>
+                            ))}
+                        </Row>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
-
+ProjectFullwidthItem.propTypes = {
+    project: PropTypes.object.isRequired,
+};
 
 export default ProjectFullwidthItem;
-
-
-
-
-// pages/index.js
-
-// import React from 'react';
-// import fullwidth from './fullwidth-itemimage';
-// import Image from 'next/image';
-// import { Col } from 'react-bootstrap';
-// const ProjectFullwidthItem = () => {
-//   return (
-//     <div>
-//       <h1>Image Gallery</h1>
-//       <Col lg={{ span: 4 }} md={{ span: 6 }}>
-//             <div className="project-item">
-//         {fullwidth.map((src, index) => (
-//           <div key={index} >
-//             <Image src={src} alt={`Image ${index + 1}`} width="200" height="200" />
-//           </div>
-//         ))}
-
-//          </div>
-//       </Col>
-//     </div>
-//   );
-// };
-
-// export default ProjectFullwidthItem;
--- src/components/projects/fullwidth-itemimage.js
+++ src/components/projects/fullwidth-itemimage.js
@@ -10,17 +10,17 @@ const fullwidth = [
 
 '/images/project-overview/overpass-road.jpg' ,
 
-'/images/project-overview/parle-palace.jpg' ,
+// '/images/project-overview/parle-palace.jpg' ,
 
 
 '/images/project-overview/pink-garden.jpg' ,
 
 
-'/images/project-overview/112.jpg',
+// '/images/project-overview/112.jpg',
 
-'/images/project-overview/113.jpg',
+// '/images/project-overview/113.jpg',
 
-'/images/project-overview/114.jpg',
+// '/images/project-overview/114.jpg',
 
 '/images/project-overview/115.jpg',
 
